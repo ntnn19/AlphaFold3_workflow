001324599e98fdbf5a87de37271945819ea34d5d
ntnn19 <natannagar89@gmail.com>
2025-06-20 14:15:40 +0200

    speedy version, works for pulldown with the optional columns

--- workflow/scripts/merge_mono_and_multi_jsons_speedy.py
+++ workflow/scripts/merge_mono_and_multi_jsons_speedy.py
@@ -39,24 +39,14 @@ def merge_jsons(multimer_data, monomer_data_list):
 
 @click.command()
 @click.argument("monomers_dir", type=click.Path())
-@click.argument("multimers_dir", type=click.Path())
+@click.argument("multimer_file", type=click.Path())
 @click.argument("output_dir",  type=click.Path())
-def main(monomers_dir,multimers_dir,output_dir):
+def main(monomers_dir,multimer_file,output_dir):
     # Separate monomeric and multimeric files
-    monomer_patterns =  os.path.join(monomers_dir,"*", "*_data.json")
-    multimer_patterns =  os.path.join(multimers_dir, "*.json")
-    monomer_files = list(glob.glob(monomer_patterns))
-    print("monomer_files=",monomer_files[:10])
-    multimer_files = list(glob.glob(multimer_patterns))
-    print("multimer_files=",multimer_files[:10])
-#    monomer_files = [p for p in json_files if "/monomers/" in p]
-#    multimer_files = [p for p in json_files if "/multimers/" in p]
+    monomer_files =  []
+    multimer_files =  [multimer_file]
+    print("multimer_file=",multimer_files)
 
-    # Create map from monomer key to JSON content
-    monomer_data = {}
-    for mf in monomer_files:
-        key = extract_monomer_key(mf)
-        monomer_data[key] = load_json(mf)
 
     # Merge monomer data into each multimer
     for multimer_path in multimer_files:
@@ -66,18 +56,19 @@ def main(monomers_dir,multimers_dir,output_dir):
         multimer_json = load_json(multimer_path)
 
         matched_monomers = []
-        for key, data in monomer_data.items():
-            for suffix in ["_auto_template_free", "_auto_template_based"]:
-                if suffix in multimer_name and key.split(suffix)[0] in multimer_name:
-                    matched_monomers.append(data)
-        click.echo(f"matched_monomers= {[m['name'] for m in matched_monomers]}")
+        suffix = "_auto_template_based" if "_auto_template_based" in multimer_name else "_auto_template_free"
+        monomer_1 = multimer_name.split(suffix)[0].split("_")[0]
+        monomer_2 = multimer_name.split(suffix)[0].split("_")[1]
+        monomer_files.append(os.path.join(monomers_dir,f"{monomer_1}{suffix}", f"{monomer_1}{suffix}_data.json"))
+        monomer_files.append(os.path.join(monomers_dir,f"{monomer_2}{suffix}", f"{monomer_2}{suffix}_data.json"))
+
+#        monomer_data = {}
+        for mf in monomer_files:
+            key = extract_monomer_key(mf)
+#            monomer_data[key] = load_json(mf)
+            matched_monomers.append(load_json(mf))
+        click.echo(f"matched_monomers= {[m for m in monomer_files]}")
         click.echo(f"multimer_json= {multimer_json}")
-        if not matched_monomers:
-            click.echo(f"Warning: No matching monomers found for {multimer_name}. Skipping...")
-            continue
-        if len(matched_monomers)==1:
-            click.echo(f"Warning: Only one monomer found for {multimer_name}. Skipping...")
-            continue
         merged = merge_jsons(multimer_json, matched_monomers)
         sub_output_dir = os.path.join(output_dir,multimer_name)
         os.makedirs(sub_output_dir, exist_ok=True)
