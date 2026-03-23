f39c72b32f57daf8f4fe1d91dc9287b0fe3c73a8
ntnn19 <natannagar89@gmail.com>
2026-03-23 15:02:00 +0100

    restored original

--- workflow/scripts/af3_script_utils.py
+++ workflow/scripts/af3_script_utils.py
@@ -1,6 +1,6 @@
 # Credit for https://github.com/hlasimpk/af3_mmseqs_scripts
 from Bio.PDB import MMCIFParser, MMCIFIO
-from Bio import Align
+from Bio import pairwise2
 from colorama import Fore, Style
 from io import StringIO
 from typing import Mapping
@@ -95,29 +95,14 @@ def query_to_hit_mapping(query_aligned: str, template_aligned: str) -> Mapping[i
             hit_index += 1
     return query_to_hit_mapping_out
 
-def do_align(ref_seq, query_seq, alignment_type="blast"): # credit to @clami66: https://github.com/clami66/AF_unmasked/blob/af3/prepare_templates_af3.py
-    if alignment_type == "blast":  # ref and query are sequences rather than structures
-        aligner = Align.PairwiseAligner()
-        aligner.substitution_matrix = Align.substitution_matrices.load("BLOSUM62")
-        aligner.open_gap_score = -0.5
-        # aligner.extend_gap_score = -0.5
-        aln = aligner.align(ref_seq, query_seq)[0]
-        print(aln[0])
-        print(aln[1])
-        try:  # compatibility between versions of Biopython
-            ref_aligned = aln[0]
-            query_aligned = aln[1]
-        except:
-            ref_aligned = aln.format().split("\n")[0]
-            query_aligned = aln.format().split("\n")[2]
-
-        return ref_aligned, query_aligned
 
 def align_and_map(query_seq, template_seq):
     """Align two sequences and map the indices."""
     # Perform pairwise alignment
-    # Perform pairwise alignment using do_align
-    query_aligned, template_aligned = do_align(template_seq, query_seq)
+    alignments = pairwise2.align.globalxx(query_seq, template_seq)
+    alignment = alignments[0]  # Take the best alignment
+    print(pairwise2.format_alignment(*alignment))
+    query_aligned, template_aligned, _, _, _ = alignment
 
     # Map the aligned sequences
     aligned_mapping = query_to_hit_mapping(query_aligned, template_aligned)
